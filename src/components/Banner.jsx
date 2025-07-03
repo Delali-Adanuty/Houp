f923cfb8962e5cdbc10d05c0124b47637abbd33a
Van-Dyck Adanuty <delaliadanuty1716@gmail.com>
2025-07-02 22:04:04 -0400

    Initial commit

---
+++ src/components/Banner.jsx
@@ -0,0 +1,12 @@
+export default function Banner(){
+    return(
+            <section className="banner">
+                <ul className="left">
+                    <h1>Houp.</h1>
+                    <p>Need a ride...  just houp!</p>
+                </ul>
+                <ul className="right">
+                </ul>
+            </section>
+    )
+}
